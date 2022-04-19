f99fe245063cda8b2f0c14212ae71a08ef1be069
NahuelDev23 <nahuel.dev.23@gmail.com>
2022-04-19 10:16:38 -0300

    Parallax 1 - terminado

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ParallaxProvider } from '../context/parallax/ParallaxProvider';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ParallaxProvider>
+      <Component {...pageProps} />
+    </ParallaxProvider>
+  )
 }
 
 export default MyApp
